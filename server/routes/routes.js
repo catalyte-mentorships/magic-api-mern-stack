a7c96894ec8264fe13b16754a0cf45990d46d18e
Perry Nguyen <pnguyen@catalyte.io>
2019-06-04 14:45:49 -0700

    Updated routes to get random card

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,18 @@
-var express = require('express');
-var router = express.Router();
-router.get('/', function(req, res){
-  res.render('index')
+const express = require('express');
+const router = express.Router();
+const mtg = require('mtgsdk');
+
+
+router.get('', (req, res) => {
+	const randomNumber = Math.floor(Math.random() * 5000) + 1;
+
+	mtg.card.find(randomNumber)
+		.then(result => {
+			res.json({
+				card: result.card,
+			});
+		})
+		.catch(err => res.send(err.message));
 });
+
 module.exports = router;
